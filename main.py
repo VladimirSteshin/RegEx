135a725e3e18d79719e09e308adb63f2674f3a06
Steshin <@gs.pmok>
2022-07-08 12:28:53 +0300

    Cleaned

--- main.py
+++ main.py
@@ -14,29 +14,25 @@ def get_list(file_name):
 def get_params(config):
     extract = configparser.ConfigParser()
     extract.read(config)
-    t_select = extract['Telephones']['select']
-    t_order = extract['Telephones']['order']
-    n_select = extract['Names']['select']
-    n_order = extract['Names']['order']
-    return t_select, t_order, n_select, n_order
+    select = extract['Telephones']['select']
+    order = extract['Telephones']['order']
+    return select, order
 
 
-def change_view(t_select, t_order, n_select, n_order, contacts):
-    t_select = r'{}'.format(t_select)
-    t_order = r'{}'.format(t_order)
-    n_select = r'{}'.format(n_select)
-    n_order = r'{}'.format(n_order)
+def change_view(select, order, contacts):
+    select = r'{}'.format(select)
+    order = r'{}'.format(order)
     changed_list = []
     for person in contacts:
-        res = re.sub(t_select, t_order, ','.join(person))
-        res = re.sub(n_select, n_order, res)
+        res = re.sub(select, order, ','.join(person))
+        res = re.sub(r'^(\w+).(\w+).(\w+)*', r'\1,\2,\3', res)
         res = re.sub(r',+', r',', res)
         res = re.sub(r' ,', r',', res)
         res = re.sub(r'[\s,]*$', r'', res)
-        res = res.split(',')
-        changed_list.append(res)
+        changed_list.append(res.split(','))
     return changed_list
 
 
 if __name__ == '__main__':
     change = change_view(*get_params('config.ini'), get_list('phonebook_raw.csv'))
+    pprint(change, width=250)
